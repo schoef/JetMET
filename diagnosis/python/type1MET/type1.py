b60726bfc0297b62b8ab88f63c78134eff8052d8
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2019-05-02 16:46:02 +0200

    tail

--- diagnosis/python/type1MET/type1.py
+++ diagnosis/python/type1MET/type1.py
@@ -1,6 +1,9 @@
 ''' Type-1 Check 
 '''
-#RootTools 
+# Standrd imports
+from math import sqrt, atan2, cos, sin
+
+# RootTools 
 from RootTools.core.standard import *
 
 # Logging
@@ -12,8 +15,9 @@ logger_rt = logger_rt.get_logger('INFO', logFile = None)
 # JECconfig
 from JetMET.JetCorrector.JetCorrector import JetCorrector, correction_levels_data, correction_levels_mc
 
-corrector_old = JetCorrector.fromTarBalls( [(1, 'Fall17_17Nov2017F_V32_DATA') ], correctionLevels = correction_levels_data )
-corrector_new = JetCorrector.fromTarBalls( [(1, 'Autumn18_RunA_V8_DATA') ], correctionLevels = correction_levels_data )
+#corrector_old     = JetCorrector.fromTarBalls( [(1, 'Fall17_17Nov2017F_V32_DATA') ], correctionLevels = correction_levels_data ) # 
+corrector_old_das = JetCorrector.fromTarBalls( [(1, 'Summer16_23Sep2016HV4_DATA') ], correctionLevels = correction_levels_data ) #this is what's used in prompt reco
+corrector_new     = JetCorrector.fromTarBalls( [(1, 'Autumn18_RunD_V8_DATA') ], correctionLevels = correction_levels_data )
 
 # products  
 products = { 
@@ -31,11 +35,24 @@ r.start()
 
 while r.run():
     print r.evt
-    print "MET", r.event.mets[0].pt()
+
+    shift_x, shift_y = 0., 0.
     for j in r.event.jets:
-        print "pt (mAOD) % 6.3f pt(Uncorr) % 6.3f pT recorr(new) % 6.3f pT recorr(old) % 6.3f" %( 
+        print "pt (mAOD) % 7.5f pt(Uncorr) % 7.5f pT recorr(new) % 7.5f pT recorr(old,DAS) % 7.5f" %( 
                 j.pt(), 
                 j.correctedJet("Uncorrected").pt(),
                 j.correctedJet("Uncorrected").pt()*corrector_new.correction( j.correctedJet("Uncorrected").pt(), j.eta(), j.jetArea(), r.event.rho[0], r.event.run ), 
-                j.correctedJet("Uncorrected").pt()*corrector_old.correction( j.correctedJet("Uncorrected").pt(), j.eta(), j.jetArea(), r.event.rho[0], r.event.run ), 
+#                j.correctedJet("Uncorrected").pt()*corrector_old.correction( j.correctedJet("Uncorrected").pt(), j.eta(), j.jetArea(), r.event.rho[0], r.event.run ), 
+                j.correctedJet("Uncorrected").pt()*corrector_old_das.correction( j.correctedJet("Uncorrected").pt(), j.eta(), j.jetArea(), r.event.rho[0], r.event.run ), 
             )
+        if j.pt()>15:
+            shift_x += j.correctedJet("Uncorrected").pt()*cos(j.correctedJet("Uncorrected").phi()) - j.pt()*cos(j.phi())
+            shift_y += j.correctedJet("Uncorrected").pt()*sin(j.correctedJet("Uncorrected").phi()) - j.pt()*sin(j.phi())
+
+    m = r.event.mets[0]
+   
+    met_type1_px = m.uncorPt()*cos(m.uncorPhi()) + shift_x
+    met_type1_py = m.uncorPt()*sin(m.uncorPhi()) + shift_y
+    met_type1 = sqrt( met_type1_px**2 + met_type1_py**2)
+    
+    print "MET(raw) % 7.5f MET (mAOD) % 7.5f MET (uncorr) % 7.5f myMET % 7.5f" % (m.uncorPt(), m.pt(), m.uncorPt(), met_type1)
